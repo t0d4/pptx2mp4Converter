1ba66a432af952d1ca06c789b30091abd18e752b
Takaaki Toda <todatakaaki@gmail.com>
2022-01-30 21:25:59 +0900

    Fixed invalid format string

--- src/version.nim
+++ src/version.nim
@@ -1,3 +1,3 @@
 # staticExec runs at compile time
 const current_version* = staticExec(
-    "nim c -r --hints:off --verbosity:0 -o:../bin/test ../util/get_version.nim pptx2mp4Converter")
+    "nim c -r --hints:off --verbosity:0 -o:../tmp/get_version ../util/get_version.nim pptx2mp4Converter")
