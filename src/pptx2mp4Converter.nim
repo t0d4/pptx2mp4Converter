ae146f59847786d4487dfa42170c1aa08c7323dc
Takaaki Toda <todatakaaki@gmail.com>
2022-01-30 15:56:51 +0900

    Implemented auto version info update

--- src/pptx2mp4Converter.nim
+++ src/pptx2mp4Converter.nim
@@ -1,5 +1,25 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 
+import parseopt2
+import version
+import zip/zipfiles
+
+let doc = """
+A simple CLI tool to convert a pptx(or ppsx) file to a mp4 video file.
+
+Usage:
+  pptx2mp4conv convert <filename>...
+  pptx2mp4conv (-h | --help)
+  pptx2mp4conv --chkdeps
+  pptx2mp4conv --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots [default: 10].
+  --moored      Moored (anchored) mine.
+  --drifting    Drifting mine.
+"""
+
 when isMainModule:
-  echo("Hello, World!")
---
+++ src/version.nim
@@ -0,0 +1,3 @@
+# staticExec runs at compile time
+const current_version* = staticExec(
+    "nim c -r --hints:off --verbosity:0 -o:../bin/test ../util/get_version.nim pptx2mp4Converter")
