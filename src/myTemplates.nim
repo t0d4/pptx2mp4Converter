17f69718686eaf53edd9919c2b70891f7b3c8dc8
Takaaki Toda <todatakaaki@gmail.com>
2022-02-05 18:50:27 +0900

    Added comment

--- src/myTemplates.nim
+++ src/myTemplates.nim
@@ -1,9 +1,11 @@
+# Visualizes the process and handles error
 template withProgressDisplayAndErrorHandling*(shouldBeSilent: bool, message: string, codeBlock: untyped): untyped =
   try:
     if not silent:
       stdout.styledWriteLine(message, styleBlink, fgYellow, " processing...", resetStyle)
     codeBlock
     if not silent:
+      # Rewrite the message, replacing "processing..." with "done."
       cursorUp(1)
       eraseLine()
       stdout.styledWriteLine(message, styleBright, fgGreen, " done", resetStyle)
